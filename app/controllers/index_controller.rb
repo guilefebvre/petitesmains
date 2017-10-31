cc805482f2fb791738714b38c2faced7fffce2c2
guilefebvre <guilefebvre@hotmail.com>
2017-10-31 16:27:45 +0000

    formulaire mail balbutiement

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,2 +1,25 @@
 class IndexController < ApplicationController
+skip_before_action :verify_authenticity_token
+
+
+	def create
+		@profil=Inscription.new(inscription_params)
+		@profil.save
+	
+	end
+	
+	
+private
+
+	def inscription_params
+		
+		params.require(:inscription).permit(:nom,:prenom, :etudiant, :email, :datenaissance)
+	end
+	
 end
+	
+
+
+	
+	
+
