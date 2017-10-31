3d56e3f86a87b30cf172d54529e8c073cc5753a6
guilefebvre <guilefebvre@hotmail.com>
2017-10-31 17:13:54 +0000

    suite

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,9 +2,15 @@ class IndexController < ApplicationController
 skip_before_action :verify_authenticity_token
 
 
+	def accueil
+	   @profil= Inscription.find_or_initialize_by(email: 'aa@aa.com')
+
+	end
+	
 	def create
 		@profil=Inscription.new(inscription_params)
 		@profil.save
+		
 	
 	end
 	
@@ -12,13 +18,11 @@ skip_before_action :verify_authenticity_token
 private
 
 	def inscription_params
-		
 		params.require(:inscription).permit(:nom,:prenom, :etudiant, :email, :datenaissance)
 	end
 	
 end
 	
-
 
 	
 	
